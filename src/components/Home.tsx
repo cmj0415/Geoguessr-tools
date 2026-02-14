72832114468b03c84ee7dfab6f27de711b31a5c7
cmj0415 <cmj51406002@gmail.com>
2026-02-14 15:08:34 +0800

    [feat] temporary home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">Welcome to GeoGuessr Tools</h1>
+            <p className="text-lg text-gray-600">Your ultimate companion for conquering GeoGuessr challenges!</p>
+        </div>
+    )
+}
+
+export default Home
