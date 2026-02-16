f3d8ae85f7bea6e5209ba3eb5ecdae3da67f160d
cmj0415 <cmj51406002@gmail.com>
2026-02-16 14:29:20 +0800

    [US States] click event

--- src/pages/USStates.tsx
+++ src/pages/USStates.tsx
@@ -7,10 +7,17 @@ import { STATE_NAME_MAP } from "../utils/USStateData";
 export default function USStates() {
     const svgRef = useRef<SVGSVGElement>(null)
 
-    const [target, setTarget] = useState(() => {
+    function pickRandomState() {
         const arr = [...STATE_NAME_MAP]
-        return arr[Math.floor(Math.random() * STATE_NAME_MAP.size)]
-    })
+        return arr[Math.floor(Math.random() * arr.length)]
+    }
+
+    const [question, setQuestion] = useState(pickRandomState())
+
+    const targetRef = useRef(question[0])
+    useEffect(() => {
+        targetRef.current = question[0]
+    }, [question])
 
     useEffect(() => {
         const svg = svgRef.current
@@ -38,19 +45,31 @@ export default function USStates() {
             target.style.fill = ""
         }
 
+        function handleClick(e: MouseEvent) {
+            const target = e.target as SVGElement
+            if (!target || target.tagName !== "path" && target.tagName !== "circle") return
+            if (target.getAttribute("class") === "separator1") return
+            const stateAbbr = target.getAttribute("class")
+            if (stateAbbr !== targetRef.current) return
+
+            setQuestion(pickRandomState())
+        }
+
         svg.addEventListener("mouseover", handleEnter)
         svg.addEventListener("mouseout", handleLeave)
+        svg.addEventListener("click", handleClick)
 
         return () => {
             svg.removeEventListener("mouseover", handleEnter)
             svg.removeEventListener("mouseout", handleLeave)
+            svg.removeEventListener("click", handleClick)
         }
     }, [])
     
     return (
         <div className="relative min-h-screen bg-slate-900">
             <h1 className="text-4xl font-bold pt-4 mb-4">US States Quiz</h1>
-            <QuestionCard target={ target[1] }/>
+            <QuestionCard target={ question[1] }/>
             <USMap className="mx-auto mt-16 w-full max-w-4xl h-auto" ref={ svgRef }/>
         </div>
     )
